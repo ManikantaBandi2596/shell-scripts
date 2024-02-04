83dc3746b8790f7258c95e28e74dbeec239e2991
manikanta_bandi <manikantabandi007@gmail.com>
2024-02-04 16:00:00 +0530

    05

---
+++ 05.variables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "please enter yout username: :"
+read USERNAME
+echo "please enter yout password: :"
+read PASSWORD
+
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
+
+
