49fbc7079d6568cb9fe9fb3b5a6a4cddada6af05
manikanta_bandi <manikantabandi007@gmail.com>
2024-02-04 15:10:25 +0530

    dates

---
+++ 04.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hi $PERSON2, good morning"
+echo "$PERSON2: hello $PERSON1, how are you?"
+echo "$PERSON1: I am fine, what about you?"
+echo "$PERSON2: pretty good $PERSON1"
