0c6320687269bfc8bf1b8872e84592086c4b2062
manikanta_bandi <manikantabandi007@gmail.com>
2024-02-08 16:28:02 +0530

    condition

--- 09.comdition.sh
+++ 09.comdition.sh
@@ -5,8 +5,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
    echo "ERROR::Please run with root access"
+   exit 1
 else
    echo "you are root user "
 fi
 
 yum install mysql -y
+if [ $? -ne 0]
+then
+   echo "error::installing mysql fallied"
+   exit 1
+else
+   echo "installed successfully"
+fi
