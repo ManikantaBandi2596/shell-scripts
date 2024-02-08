2dd47ce902ee426ed7aa0564f0379cb2adc87d89
manikanta_bandi <manikantabandi007@gmail.com>
2024-02-08 18:41:58 +0530

    condition

---
+++ 10.condition.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "error::installing  fallied"
+        exit 1
+    else
+        echo " installed successfully"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+   echo "ERROR::Please run with root access"
+   exit 1
+else
+   echo "you are root user "
+fi
+
+yum install mysql -y
+VALIDATE
+
+yum install nginx1 -y
+VALIDATE
